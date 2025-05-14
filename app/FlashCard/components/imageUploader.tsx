f73c0f26e573ad0ad497ea6aeb59fcc126668212
mailee-dizon <mailee.dizon05@gmail.com>
2025-05-14 16:50:06 -0700

    debug

--- app/FlashCard/components/imageUploader.tsx
+++ app/FlashCard/components/imageUploader.tsx
@@ -39,7 +39,7 @@ export default function ImageUploader( {userId, deckId, onUploadComplete} : Imag
             <input type="file" onChange={handleImageChange}/>
             <button onClick={handleUpload}>Upload</button>
             {imageUploaded ? (
-                <Image src={imageUrl} width="20" height="20" alt="image"/>
+                <Image src={"https://firebasestorage.googleapis.com/v0/b/flipcards-448722.firebasestorage.app/o/images%2FaTmxw9m61VURvUuceomo3SKDVnC3%2Ft%2F9th-amendment.jpeg?alt=media&token=93464555-47ed-476d-9be2-b869aa3a95b3"} width="20" height="20" alt="image"/>
             ) : (
                 <div></div>
             )}
