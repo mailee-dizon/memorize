f334380283a2924ee75ad03be40a3c5e0faff6fc
mailee-dizon <mailee.dizon05@gmail.com>
2025-05-14 17:55:01 -0700

    debug

--- app/FlashCard/utils/uploadImage.tsx
+++ app/FlashCard/utils/uploadImage.tsx
@@ -2,7 +2,7 @@ import { ref, uploadBytes, getDownloadURL } from "firebase/storage"
 import { storage } from "@/app/firebase/config"
 
 async function uploadImage (file: File, userId: string, deckId: string) : Promise<string> {
-    const storageRef = ref(storage, `gs://flipcards-448722.firebasestorage.app/images/${userId}/${deckId}/${file.name}`)
+    const storageRef = ref(storage, `images/${userId}/${deckId}/${file.name}`)
     const snapshot = await uploadBytes(storageRef, file)
     return await getDownloadURL(snapshot.ref)
 }
